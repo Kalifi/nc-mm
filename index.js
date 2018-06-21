076830060093fc85eec7281fd1af76212ac3dacc
Wille <wille.keltikangas@gofore.com>
2018-06-21 12:28:11 +0300

    Show next match and estimated scores

--- index.js
+++ index.js
@@ -3,37 +3,45 @@ const urls = {
   profile: 'https://yle.fi/urheilu/3-10240157#/user/',
   rank: 'https://yle-futistietaja.herokuapp.com/api/rank/',
   matches: 'https://yle-futistietaja.herokuapp.com/api/matches',
-  matchResults: 'https://yle-futistietaja.herokuapp.com/api/matchresults/'
+  matchResults: 'https://yle-futistietaja.herokuapp.com/api/matchresults/',
+  answer: 'https://yle-futistietaja.herokuapp.com/api/answer/'
 }
 const table = document.getElementById('results');
+const guessTable = document.getElementById('guesses');
 
 function fetchMatches() {
    return fetch(urls.matches)
     .then(res => res.json())
     .then(data => data.reduce((acc, group) => {
-      const groupMatches = group.matches.filter(match => match.status === 'finished');
-      return acc.concat(groupMatches);
-    }, []))
+      for (let match of group.matches) {
+        if (match.status === 'finished')
+          acc[0].push(match);
+        else
+          acc[1].push(match);
+      }
+      return acc;
+    }, [[], []]))
     .catch(e => console.error(e));
-}
-
-function parseMatches(matches) {
-  return matches.reduce((acc, group) => {
-    const groupMatches = group.matches.filter(match => match.status === 'finished');
-    return acc.concat(groupMatches);
-  }, []);
 }
 
 function fetchPoints(nickname) {
   return fetch(urls.matchResults + nickname)
     .then(res => res.json())
-    .then(data => {
-      return {
-        name: data.nickname,
-        points: data.matchresults.map(p => ({id: p[0], points: p[1]}))
-      }
-    })
+    .then(data => ({
+      name: data.nickname,
+      points: data.matchresults.map(p => ({id: p[0], points: p[1]}))
+    }))
     .catch(e => fetchPoints(nickname)); //retry
+}
+
+function fetchAnswers(nickname) {
+  return fetch(urls.answer + nickname)
+    .then(res => res.json())
+    .then(data => ({
+      name: data.nickname,
+      goals: data.values.matchTeamGoals
+    }))
+    .catch(e => console.error(e));
 }
 
 function parsePoints(matchresults) {
@@ -66,18 +74,34 @@ function addTableRow(nickname, points) {
   pointsCell.innerHTML = points;
 }
 
+function addGuessRow(nickname, home, guest) {
+  const row = guessTable.insertRow();
+  const nicknameCell = row.insertCell();
+  nicknameCell.innerHTML = nickname;
+
+  const scoreCell = row.insertCell();
+  scoreCell.innerHTML = home + ' - ' + guest;
+}
+
 Promise.all(participants.map(nickname => fetchDetails(nickname)))
   .then(list => list.forEach(obj => addTableRow(obj.nickname, obj.points)));
 
-Promise.all([fetchMatches(), Promise.all(participants.map(nickname => fetchPoints(nickname)))])
-  .then(([matches, points]) => {
-    const sortedMatches = matches.sort((a, b) => new Date(a.date) - new Date(b.date))
+Promise.all([fetchMatches(), Promise.all(participants.map(nickname => fetchPoints(nickname))), Promise.all(participants.map(nickname => fetchAnswers(nickname)))])
+  .then(([matches, points, answers]) => {
+    const sortedMatches = matches[0].sort((a, b) => new Date(a.date) - new Date(b.date))
       .reduce((acc, match) => {
         acc[0].push(match.id);
         acc[1].push(match.name);
         return acc;
       }, [[], []]);
     
+    const nextMatch = matches[1].sort((a, b) => new Date(a.date) - new Date(b.date))[0];
+    document.getElementById('next-match').innerHTML = nextMatch.name;
+    answers.map(participant => {
+      const guesses = participant.goals.filter(i => i.matchId === nextMatch.id);
+      addGuessRow(participant.name, guesses[0].goals, guesses[1].goals);
+    })
+
     points.map(participant => {
       let sortedPoints = [];
       for(const id of sortedMatches[0]) {
